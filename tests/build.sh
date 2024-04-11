08bac3b441ac485d5c1404088b3839ad1d08a0d2
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-11 08:59:54 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/jgraph/docker-drawio.git docker
 rm -f ./docker/README.md
 mv ./docker/image-export/Dockerfile ./Dockerfile
+sed -i "s~EXPOSE 8000~EXPOSE 8080~g" ./Dockerfile
 
 
 docker buildx build . --output type=docker,name=elestio4test/drawio:latest | docker load
