d55b9570bc74380a5d7430ae8f2a271e0d0807d1
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-21 15:06:41 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/jgraph/docker-drawio.git docker
 rm -f ./docker/README.md
-mv ./docker/image-export/Dockerfile ./Dockerfile
+mv ./docker/main/Dockerfile ./Dockerfile
 sed -i "s~EXPOSE 8000~EXPOSE 8080~g" ./Dockerfile
 
 
